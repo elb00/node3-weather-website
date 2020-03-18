87ce99e3bd716b1043834fc9dc17935425f33938
Erica Baron <ericabaron@earthlink.net>
2020-03-18 12:56:34 -0600

    Add data to forecast string and change pic on About page

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,9 +9,10 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error){
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + 
-            body.currently.temperature +
-            ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' The high for today is ' + 
+            body.daily.data[0].temperatureHigh + ' degrees F and the low is ' + body.daily.data[0].temperatureLow + " degrees. " +
+            'There is a ' + parseInt(body.daily.data[0].precipProbability * 100) + '% chance of rain.  ' +
+            'It is currently ' + body.currently.temperature + ' degrees F')
         }
     })
 
